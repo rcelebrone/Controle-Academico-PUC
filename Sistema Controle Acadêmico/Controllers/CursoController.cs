caec4128c15e8c18814b29cdbc2ad2c2a74a29f8
Rodrigo <rcelebrone@gmail.com>
2017-07-30 19:56:59 -0300

    Add project files.

---
+++ Sistema Controle Acadêmico/Controllers/CursoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace sca.Controllers
+{
+    public class CursoController : Controller
+    {
+        [OutputCache(Duration = (60 * 60 * 24), Location = OutputCacheLocation.Server)]
+        public ActionResult Listagem()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sistema Controle Acadêmico/Controllers/MatriculaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace sca.Controllers
+{
+    public class MatriculaController : Controller
+    {
+        //[OutputCache(Duration = (60*60*24), Location = OutputCacheLocation.Server)]
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
